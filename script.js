4a756ae7d1ec7c30996adf44e68729d0d653e987
Vyshnav <vyshnavk78@gmail.com>
2023-08-31 16:05:05 +0530

    added a reveal joke button for twotype joke

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const jokeBox = document.getElementById("joke");
+const jokeDelivery = document.getElementById("joke-delivery");
 const btn = document.getElementById("btn");
-const url = "https://v2.jokeapi.dev/joke/Any"
+const btn1 = document.getElementById("btn1");
+const url = "https://v2.jokeapi.dev/joke/Any";
 
-let getJoke = ()=>{
+let currentJoke;
+let getJoke = () => {
     fetch(url)
     .then(data => data.json())
-    .then(item =>{
-        if(item.type === "single"){
-            jokeBox.textContent = `${item.joke}`
+    .then(item => {
+        currentJoke = item;
+        if (item.type === "twopart") {
+            jokeBox.style.display = "inherit";
+            jokeDelivery.textContent = ""; 
+            jokeBox.textContent = `${item.setup}`;
+            btn1.style.display = "inline";
+        } else if (item.type === "single") {
+            btn1.style.display = "none";
+            jokeBox.textContent = `${item.joke}`;
+            jokeDelivery.textContent = ""; 
         }
-        jokeBox.textContent = `${item.setup}  ${item.delivery}`
     })
-    .catch(err=>{
+    .catch(err => {
         console.error(err);
     });
 }
 
+let showJoke=()=>{
+    jokeBox.style.display = "none";
+    jokeDelivery.textContent = `${currentJoke.delivery}`;
+    console.log(currentJoke.delivery);
+}
 btn.addEventListener("click", getJoke);
+btn1.addEventListener("click", showJoke);
+// getJoke();
